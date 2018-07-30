7be918abca6ceaaac70174fc5ba1034ca4e29d9d
Michele Beltrame <mb@italpro.net>
2018-07-30 16:51:34 +0200

    Basic implementation done

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,61 @@
 //! countries. This crate works with **EU VAT IDs only**.
 
 #[macro_use] extern crate failure;
+#[macro_use] extern crate lazy_static;
+extern crate regex;
 
 use failure::Error;
+use regex::Regex;
+use std::collections::HashMap;
+
+/// Struct containing VAT ID and some information about it
+#[derive(Debug, PartialEq)]
+pub struct VATID {
+    pub state_iso    : String,
+    pub local_vat_id : String
+}
+
+lazy_static! {
+    static ref RX_GENERAL : Regex = Regex::new("^([A-Z]{2})(.+)$").unwrap(); // FIXME: limit char variants after country
+
+    // Reference: http://ec.europa.eu/taxation_customs/vies/faq.html#item_11
+    static ref RXS_COUNTRIES: HashMap<String, Regex> = {
+        let mut m = HashMap::new();
+        m.insert(String::from("AT"), Regex::new(r"^U[0-9]{8}$").unwrap());
+        m.insert(String::from("BE"), Regex::new(r"^0[0-9]{9}$").unwrap());
+        m.insert(String::from("BG"), Regex::new(r"^[0-9]{9,10}$").unwrap());
+        m.insert(String::from("CY"), Regex::new(r"^[0-9]{8}[A-Za-z]$").unwrap());
+        m.insert(String::from("CZ"), Regex::new(r"^[0-9]{8,10}$").unwrap());
+        m.insert(String::from("DE"), Regex::new(r"^[0-9]{9}$").unwrap());
+        m.insert(String::from("DK"), Regex::new(r"^[0-9]{2} ?[0-9]{2} ?[0-9]{2} ?[0-9]{2}$").unwrap());
+        m.insert(String::from("EE"), Regex::new(r"^[0-9]{9}$").unwrap());
+        m.insert(String::from("EL"), Regex::new(r"^[0-9]{9}$").unwrap());
+        m.insert(String::from("ES"), Regex::new(r"^[A-Za-z0-9][0-9]{7}[A-Za-z0-9]$").unwrap());
+        m.insert(String::from("FI"), Regex::new(r"^[0-9]{8}$").unwrap());
+        m.insert(String::from("FR"), Regex::new(r"^[A-Za-z0-9]{2} ?[0-9]{9}$").unwrap());
+        m.insert(String::from("GB"), Regex::new(r"^[0-9]{3} ?[0-9]{4} ?[0-9]{2}|[0-9]{3} ?[0-9]{4} ?[0-9]{2} ?[0-9]{3}|GD[0-9]{3}|HA[0-9]{3}$").unwrap());
+        m.insert(String::from("HR"), Regex::new(r"^[0-9]{11}$").unwrap());
+        m.insert(String::from("IT"), Regex::new(r"^[0-9]{11}$").unwrap());
+        m.insert(String::from("HU"), Regex::new(r"^[0-9]{8}$").unwrap());
+        m.insert(String::from("IE"), Regex::new(r"^[0-9][A-Za-z0-9\+\*][0-9]{5}[A-Za-z]{1,2}$").unwrap());
+        m.insert(String::from("LT"), Regex::new(r"^[0-9]{9}|[0-9]{12}$").unwrap());
+        m.insert(String::from("LU"), Regex::new(r"^[0-9]{8}$").unwrap());
+        m.insert(String::from("LV"), Regex::new(r"^[0-9]{11}$").unwrap());
+        m.insert(String::from("MT"), Regex::new(r"^[0-9]{8}$").unwrap());
+        m.insert(String::from("NL"), Regex::new(r"^[0-9]{9}B[0-9]{2}$").unwrap());
+        m.insert(String::from("PL"), Regex::new(r"^[0-9]{10}$").unwrap());
+        m.insert(String::from("PT"), Regex::new(r"^[0-9]{9}$").unwrap());
+        m.insert(String::from("RO"), Regex::new(r"^[0-9]{2,10}$").unwrap());
+        m.insert(String::from("SE"), Regex::new(r"^[0-9]{12}$").unwrap());
+        m.insert(String::from("SI"), Regex::new(r"^[0-9]{8}$").unwrap());
+        m.insert(String::from("SK"), Regex::new(r"^[0-9]{10}$").unwrap());
+        m
+    };
+}
 
 /// Check if a VAT ID has a valid syntax. It's an offline check, so it doesn't guarantee the VAT ID exists.
+/// 
+/// The function is case insensitive
 ///
 /// # Examples
 /// 
@@ -23,31 +74,52 @@ use failure::Error;
 /// ```
 pub fn check(vat_id : &str) -> bool {
     match parse(vat_id) {
-        Ok(true)    => true,
-        Ok(false)   => false,
+        Ok(_vi)     => true,
         Err(_e)     => false
     }
 }
 
 /// Parses a VAT ID to see if it has a valid syntax. It's an offline check, so it doesn't guarantee the VAT ID exists.
-/// In case of success the function returns a bool which is always *true*, otherwise returns an `Error`.
+/// In case of success the function returns a `VATID` struct, otherwise returns an `Error`.
 /// 
+/// The function is case insensitive
+///
 /// You should only use this function if you need to know why the VAT ID is not valid, otherwise please use `check()`.
 ///
 /// # Examples
 /// 
 /// ```
 /// match eu_vat_id::parse("IT00400770939") {
-///     Ok(_bool)   => println!("VAT ID is valid"), // Don't care if it's true or false
-///     Err(e)      => println!("Codice is invalid beacuse: {:?}", e),    
+///     Ok(vi)      => println!("VAT ID is valid, state is {}", vi.state_iso),
+///     Err(e)      => println!("VAT ID is invalid because: {:?}", e),    
 /// }
 /// ```
 /// 
 /// # Errors
 /// 
 /// * *invalid-base-structure* - VAT ID is not 2 letters + ??
-/// * *invalid-country* - not an EU country
-/// * *invalid-vat_id* - the VAT ID structure is not valid for the country specified
-pub fn parse(vat_id : &str) -> Result<bool, Error> {
-    Ok(true)
+/// * *invalid-state* - not an EU state
+/// * *invalid-local_vat_id* - the VAT ID structure is not valid for the state specified
+pub fn parse(vat_id : &str) -> Result<VATID, Error> {
+    let (state_iso, local_vat_id) = match RX_GENERAL.captures(&vat_id.to_uppercase()) {
+        Some(cp)    => (
+            String::from( cp.get(1).unwrap().as_str() ),
+            String::from( cp.get(2).unwrap().as_str() )
+        ),
+        None        => bail!("invalid-base-structure")
+    };
+
+    let rx_local = match RXS_COUNTRIES.get(&state_iso) {
+        Some(rx)    => rx,
+        None        => bail!("invalid-state")
+    };
+
+    if !rx_local.is_match(&local_vat_id) {
+        bail!("invalid-local_vat_id");
+    }
+
+    Ok(VATID {
+        state_iso   : state_iso,
+        local_vat_id: local_vat_id
+    })
 }
--- tests/basic.rs
+++ tests/basic.rs
@@ -2,6 +2,44 @@
 extern crate eu_vat_id;
 
 const TEST_IT_OK            : &str = "IT00400770939";
+const TEST_INVALID          : &str = "3_4524DE";
+const TEST_INVALID_STATE    : &str = "XX1234";
+const TEST_INVALID_LOCAL    : &str = "IT00400";
+
+#[test]
+fn t_parse_ok() {
+    assert_eq!(
+        eu_vat_id::parse(TEST_IT_OK).unwrap(),
+        eu_vat_id::VATID {
+            state_iso       : String::from("IT"),
+            local_vat_id    : String::from("00400770939")
+        }
+    );
+}
+
+#[test]
+fn t_parse_invalid() {
+    assert_eq!(
+        format!("{}",eu_vat_id::parse(TEST_INVALID).err().unwrap()),
+        "invalid-base-structure"
+    );
+}
+
+#[test]
+fn t_parse_invalid_state() {
+    assert_eq!(
+        format!("{}",eu_vat_id::parse(TEST_INVALID_STATE).err().unwrap()),
+        "invalid-state"
+    );
+}
+
+#[test]
+fn t_parse_invalid_local() {
+    assert_eq!(
+        format!("{}",eu_vat_id::parse(TEST_INVALID_LOCAL).err().unwrap()),
+        "invalid-local_vat_id"
+    );
+}
 
 #[test]
 fn t_check_ok() {
