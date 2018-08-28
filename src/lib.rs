5a2b52819170fd3610d15d44b9cf3aeb728b89d2
Michele Beltrame <mb@italpro.net>
2018-08-28 16:44:00 +0200

    Processed files with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,24 @@
 //! This crate provides validation of European Union *VAT IDs*, which are
 //! (basically, even though there are exceptions) the company registration
 //! numbers within the EU.
-//! 
+//!
 //! Please have a look at the function definitions for usage examples.
-//! 
+//!
 //! All countries which have Value Added Tax have VAT IDs, so not only
 //! European Union states. This crate works with **EU VAT IDs only**.
-//! 
+//!
 //! We implement and pass all tests for the sample VIES IDs:
 //! [http://ec.europa.eu/taxation_customs/vies/faq.html#item_11](http://ec.europa.eu/taxation_customs/vies/faq.html#item_11)
-//! 
+//!
 //! ## TODO
-//! 
+//!
 //! * Make regulax expressions more specific so they can check more state-specific things such are check digits.
 //! * Add interface with VIES to provide, at which, an online verification of the existence of a VAT ID. *This may be a stand-alone crate, as it will need to pull in a few dependencies and I'd prefer this crate to stay light*.
 
-
-#[macro_use] extern crate failure;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate lazy_static;
 extern crate regex;
 
 use failure::Error;
@@ -27,8 +28,8 @@ use std::collections::HashMap;
 /// Struct containing VAT ID and some information about it
 #[derive(Debug, PartialEq)]
 pub struct VATID {
-    pub state_iso    : String,
-    pub local_vat_id : String
+    pub state_iso: String,
+    pub local_vat_id: String,
 }
 
 lazy_static! {
@@ -70,11 +71,11 @@ lazy_static! {
 }
 
 /// Check if a VAT ID has a valid syntax. It's an offline check, so it doesn't guarantee the VAT ID exists.
-/// 
+///
 /// The function is case insensitive and expects a VAT ID beginning with the 2-letter EU state ISO code.
 ///
 /// # Examples
-/// 
+///
 /// ```
 /// if eu_vat_id::check("IT00400770939") == true {
 ///     println!("VAT ID is valid");
@@ -82,57 +83,57 @@ lazy_static! {
 /// ```
 pub fn check(vat_id: &str) -> bool {
     match parse(vat_id) {
-        Ok(_vi)     => true,
-        Err(_e)     => false
+        Ok(_vi) => true,
+        Err(_e) => false,
     }
 }
 
 /// Same as `check`, but expects a "local" VAT ID, with the state ISO code specified separately
 ///
 /// # Examples
-/// 
+///
 /// ```
 /// if eu_vat_id::check_by_state("00400770939", "IT") == true {
 ///     println!("VAT ID is valid");
 /// }
 /// ```
 pub fn check_by_state(local_vat_id: &str, state: &str) -> bool {
-    check( &format!("{}{}", state, local_vat_id) )
+    check(&format!("{}{}", state, local_vat_id))
 }
 
 /// Parses a VAT ID to see if it has a valid syntax. It's an offline check, so it doesn't guarantee the VAT ID exists.
 /// In case of success the function returns a `VATID` struct, otherwise returns an `Error`.
-/// 
+///
 /// The function is case insensitive
 ///
 /// You should only use this function if you need to know why the VAT ID is not valid, otherwise please use `check()`.
 ///
 /// # Examples
-/// 
+///
 /// ```
 /// match eu_vat_id::parse("IT00400770939") {
 ///     Ok(vi)      => println!("VAT ID is valid, state is {}", vi.state_iso),
 ///     Err(e)      => println!("VAT ID is invalid because: {:?}", e),    
 /// }
 /// ```
-/// 
+///
 /// # Errors
-/// 
+///
 /// * *invalid-base-structure* - VAT ID is not 2 letters + ??
 /// * *invalid-state* - not an EU state
 /// * *invalid-local_vat_id* - the VAT ID structure is not valid for the state specified
-pub fn parse(vat_id : &str) -> Result<VATID, Error> {
+pub fn parse(vat_id: &str) -> Result<VATID, Error> {
     let (state_iso, local_vat_id) = match RX_GENERAL.captures(&vat_id.to_uppercase()) {
-        Some(cp)    => (
-            String::from( cp.get(1).unwrap().as_str() ),
-            String::from( cp.get(2).unwrap().as_str() )
+        Some(cp) => (
+            String::from(cp.get(1).unwrap().as_str()),
+            String::from(cp.get(2).unwrap().as_str()),
         ),
-        None        => bail!("invalid-base-structure")
+        None => bail!("invalid-base-structure"),
     };
 
     let rx_local = match RXS_COUNTRIES.get(&state_iso) {
-        Some(rx)    => rx,
-        None        => bail!("invalid-state")
+        Some(rx) => rx,
+        None => bail!("invalid-state"),
     };
 
     if !rx_local.is_match(&local_vat_id) {
@@ -140,7 +141,7 @@ pub fn parse(vat_id : &str) -> Result<VATID, Error> {
     }
 
     Ok(VATID {
-        state_iso   : state_iso,
-        local_vat_id: local_vat_id
+        state_iso: state_iso,
+        local_vat_id: local_vat_id,
     })
 }
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,18 +1,49 @@
 #![cfg(test)]
 extern crate eu_vat_id;
 
-const TEST_IT_OK            : &str = "IT00400770939";
-const TEST_IT_OK_STATE      : &str = "IT";
-const TEST_IT_OK_LOCAL      : &str = "00400770939";
-const TEST_INVALID          : &str = "3_4524DE";
-const TEST_INVALID_STATE    : &str = "XX1234";
-const TEST_INVALID_LOCAL    : &str = "IT00400";
-const TEST_VIESFAQ_OK       : &[&str] = &[
-    "ATU99999999", "BE0999999999", "BG999999999", "BG9999999999", "CY99999999L", "CZ99999999", "CZ999999999",
-    "CZ9999999999", "DE999999999", "DK99 99 99 99", "EE999999999", "EL999999999", "ESX9999999X", "FI99999999",
-    "FRXX 999999999", "GB999 9999 99", "GB999 9999 99 999", "GBGD999", "GBHA999", "HR99999999999", "HU99999999",
-    "IE9S99999L", "IE9999999WI", "IT99999999999", "LT999999999", "LT999999999999", "LU99999999", "LV99999999999",
-    "MT99999999", "NL999999999B99", "PL9999999999", "PT999999999", "RO999999999", "SE999999999999", "SI99999999", "SK9999999999"
+const TEST_IT_OK: &str = "IT00400770939";
+const TEST_IT_OK_STATE: &str = "IT";
+const TEST_IT_OK_LOCAL: &str = "00400770939";
+const TEST_INVALID: &str = "3_4524DE";
+const TEST_INVALID_STATE: &str = "XX1234";
+const TEST_INVALID_LOCAL: &str = "IT00400";
+const TEST_VIESFAQ_OK: &[&str] = &[
+    "ATU99999999",
+    "BE0999999999",
+    "BG999999999",
+    "BG9999999999",
+    "CY99999999L",
+    "CZ99999999",
+    "CZ999999999",
+    "CZ9999999999",
+    "DE999999999",
+    "DK99 99 99 99",
+    "EE999999999",
+    "EL999999999",
+    "ESX9999999X",
+    "FI99999999",
+    "FRXX 999999999",
+    "GB999 9999 99",
+    "GB999 9999 99 999",
+    "GBGD999",
+    "GBHA999",
+    "HR99999999999",
+    "HU99999999",
+    "IE9S99999L",
+    "IE9999999WI",
+    "IT99999999999",
+    "LT999999999",
+    "LT999999999999",
+    "LU99999999",
+    "LV99999999999",
+    "MT99999999",
+    "NL999999999B99",
+    "PL9999999999",
+    "PT999999999",
+    "RO999999999",
+    "SE999999999999",
+    "SI99999999",
+    "SK9999999999",
 ];
 
 #[test]
@@ -20,8 +51,8 @@ fn t_parse_ok() {
     assert_eq!(
         eu_vat_id::parse(TEST_IT_OK).unwrap(),
         eu_vat_id::VATID {
-            state_iso       : String::from(TEST_IT_OK_STATE),
-            local_vat_id    : String::from(TEST_IT_OK_LOCAL)
+            state_iso: String::from(TEST_IT_OK_STATE),
+            local_vat_id: String::from(TEST_IT_OK_LOCAL)
         }
     );
 }
@@ -29,7 +60,7 @@ fn t_parse_ok() {
 #[test]
 fn t_parse_invalid() {
     assert_eq!(
-        format!("{}",eu_vat_id::parse(TEST_INVALID).err().unwrap()),
+        format!("{}", eu_vat_id::parse(TEST_INVALID).err().unwrap()),
         "invalid-base-structure"
     );
 }
@@ -37,7 +68,7 @@ fn t_parse_invalid() {
 #[test]
 fn t_parse_invalid_state() {
     assert_eq!(
-        format!("{}",eu_vat_id::parse(TEST_INVALID_STATE).err().unwrap()),
+        format!("{}", eu_vat_id::parse(TEST_INVALID_STATE).err().unwrap()),
         "invalid-state"
     );
 }
@@ -45,7 +76,7 @@ fn t_parse_invalid_state() {
 #[test]
 fn t_parse_invalid_local() {
     assert_eq!(
-        format!("{}",eu_vat_id::parse(TEST_INVALID_LOCAL).err().unwrap()),
+        format!("{}", eu_vat_id::parse(TEST_INVALID_LOCAL).err().unwrap()),
         "invalid-local_vat_id"
     );
 }
@@ -57,7 +88,10 @@ fn t_check_ok() {
 
 #[test]
 fn t_check_by_state_ok() {
-    assert_eq!(eu_vat_id::check_by_state(TEST_IT_OK_LOCAL, TEST_IT_OK_STATE), true);
+    assert_eq!(
+        eu_vat_id::check_by_state(TEST_IT_OK_LOCAL, TEST_IT_OK_STATE),
+        true
+    );
 }
 
 #[test]
@@ -67,4 +101,3 @@ fn t_check_ok_viesfaq() {
         assert_eq!(eu_vat_id::check(vq), true);
     }
 }
-
